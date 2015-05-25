27c9e9c1c716ffb0f24b8ef39533d3a4be56062e
Andreas Karlsson <andreas@proxel.se>
2015-05-25 19:37:29 +0200

    Use secure_compare when checking CSRF token
    Since string comparisions may return early we want to use a constant
    time comparsion function to protect the CSRF token against timing
    attacks. Rack::Utils provides a such function.

--- lib/rack/protection/base.rb
+++ lib/rack/protection/base.rb
@@ -1,4 +1,5 @@
 require 'rack/protection'
+require 'rack/utils'
 require 'digest'
 require 'logger'
 require 'uri'
@@ -99,6 +100,10 @@ module Rack
 
       def encrypt(value)
         options[:encryptor].hexdigest value.to_s
+      end
+
+      def secure_compare(a, b)
+        Rack::Utils.secure_compare(a.to_s, b.to_s)
       end
 
       alias default_reaction deny
